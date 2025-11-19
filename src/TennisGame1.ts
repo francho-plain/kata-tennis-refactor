0636f10d63b98551a43d03e0a31690ee4a91f93d
Francho Joven <fjoven@plainconcepts.com>
2025-11-19 12:04:07 +0100

    refactor: rename score variables for clarity and consistency

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -5,8 +5,8 @@ const WINNING_SCORE: number = 3;
 
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
+  private player1Score: number = 0;
+  private player2Score: number = 0;
   private readonly player1Name: string;
   private readonly player2Name: string;
 
@@ -17,23 +17,21 @@ export class TennisGame1 implements TennisGame {
 
   wonPoint(playerName: string): void {
     if (playerName === this.player1Name) {
-      this.m_score1 += 1;
-    }
-
-    if (playerName === this.player2Name) {
-      this.m_score2 += 1;
+      this.player1Score += 1;
+    } else if (playerName === this.player2Name) {
+      this.player2Score += 1;
     }
   }
 
   getScore(): string {
-    return this.getEvenScore(this.m_score1)
-      || this.getAdvantageOrWinScore(this.m_score1, this.m_score2)
-      || this.getNormalScore(this.m_score1, this.m_score2);
+    return this.getEvenScore(this.player1Score)
+      || this.getAdvantageOrWinScore(this.player1Score, this.player2Score)
+      || this.getNormalScore(this.player1Score, this.player2Score);
   }
 
-  private getEvenScore(score: number): string | false {
-    if (this.m_score1 !== this.m_score2) {
-      return false
+  private getEvenScore(score: number): string | null {
+    if (this.player1Score !== this.player2Score) {
+      return null;
     }
 
     return score < WINNING_SCORE ? `${EVEN_SCORES[score]}-All` : 'Deuce';
@@ -47,15 +45,15 @@ export class TennisGame1 implements TennisGame {
     return EVEN_SCORES[score] || '';
   }
 
-  private getAdvantageOrWinScore(score1: number, score2: number): string | false {
+  private getAdvantageOrWinScore(score1: number, score2: number): string | null {
 
-    if (this.m_score1 <= WINNING_SCORE && this.m_score2 <= WINNING_SCORE) {
-      return false
+    if (this.player1Score <= WINNING_SCORE && this.player2Score <= WINNING_SCORE) {
+      return null;
     }
     const minusResult = score1 - score2;
-    if (minusResult === 1) { return 'Advantage player1'; }
-    if (minusResult === -1) { return 'Advantage player2'; }
-    if (minusResult >= 2) { return 'Win for player1'; }
-    return 'Win for player2';
+    if (minusResult === 1) { return `Advantage ${this.player1Name}`; }
+    if (minusResult === -1) { return `Advantage ${this.player2Name}`; }
+    if (minusResult >= 2) { return `Win for ${this.player1Name}`; }
+    return `Win for ${this.player2Name}`;
   }
 }
