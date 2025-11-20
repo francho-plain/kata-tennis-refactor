80520e507009145e2129a715ba9e4a704b05b73d
Francho Joven <fjoven@plainconcepts.com>
2025-11-20 11:17:43 +0100

    refactor: enhance score representation and improve conditional formatting in getScore method

--- src/TennisGame2.ts
+++ src/TennisGame2.ts
@@ -18,60 +18,78 @@ export class TennisGame2 implements TennisGame {
   getScore(): string {
     let score: string = '';
     if (this.P1point === this.P2point && this.P1point < 4) {
-      if (this.P1point === 0)
+      if (this.P1point === 0) {
         score = 'Love';
-      if (this.P1point === 1)
+      }
+      if (this.P1point === 1) {
         score = 'Fifteen';
-      if (this.P1point === 2)
+      }
+      if (this.P1point === 2) {
         score = 'Thirty';
+      }
       score += '-All';
     }
-    if (this.P1point === this.P2point && this.P1point >= 3)
+    if (this.P1point === this.P2point && this.P1point >= 3) {
       score = 'Deuce';
+    }
 
     if (this.P1point > 0 && this.P2point === 0) {
-      if (this.P1point === 1)
+      if (this.P1point === 1) {
         this.P1res = 'Fifteen';
-      if (this.P1point === 2)
+      }
+      if (this.P1point === 2) {
         this.P1res = 'Thirty';
-      if (this.P1point === 3)
+      }
+      if (this.P1point === 3) {
         this.P1res = 'Forty';
+      }
 
       this.P2res = 'Love';
       score = this.P1res + '-' + this.P2res;
     }
     if (this.P2point > 0 && this.P1point === 0) {
-      if (this.P2point === 1)
+      if (this.P2point === 1) {
         this.P2res = 'Fifteen';
-      if (this.P2point === 2)
+      }
+      if (this.P2point === 2) {
         this.P2res = 'Thirty';
-      if (this.P2point === 3)
+      }
+      if (this.P2point === 3) {
         this.P2res = 'Forty';
+      }
 
       this.P1res = 'Love';
       score = this.P1res + '-' + this.P2res;
     }
 
     if (this.P1point > this.P2point && this.P1point < 4) {
-      if (this.P1point === 2)
+      if (this.P1point === 2) {
         this.P1res = 'Thirty';
-      if (this.P1point === 3)
+      }
+      if (this.P1point === 3) {
         this.P1res = 'Forty';
-      if (this.P2point === 1)
+      }
+      if (this.P2point === 1) {
         this.P2res = 'Fifteen';
-      if (this.P2point === 2)
+      }
+      if (this.P2point === 2) {
         this.P2res = 'Thirty';
+      }
       score = this.P1res + '-' + this.P2res;
     }
     if (this.P2point > this.P1point && this.P2point < 4) {
-      if (this.P2point === 2)
+      if (this.P2point === 2) {
         this.P2res = 'Thirty';
-      if (this.P2point === 3)
+      }
+      if (this.P2point === 3) {
         this.P2res = 'Forty';
-      if (this.P1point === 1)
+      }
+      if (this.P1point === 1) {
         this.P1res = 'Fifteen';
-      if (this.P1point === 2)
+      }
+      if (this.P1point === 2) {
         this.P1res = 'Thirty';
+      }
       score = this.P1res + '-' + this.P2res;
     }
 
@@ -113,9 +131,7 @@ export class TennisGame2 implements TennisGame {
   }
 
   wonPoint(player: string): void {
-    if (player === 'player1')
-      this.P1Score();
-    else
-      this.P2Score();
+    if (player === 'player1') { this.P1Score(); }
+    else { this.P2Score(); }
   }
 }
