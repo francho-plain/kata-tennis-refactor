2e388067914a675f782cb2e5637b3f418d7d4e7a
Francho Joven <fjoven@plainconcepts.com>
2025-12-04 12:00:13 +0100

    refactor: enhance score handling by introducing buildScore method and restructuring lookup logic in TennisGame5

--- src/TennisGame5.ts
+++ src/TennisGame5.ts
@@ -1,16 +1,28 @@
 import { TennisGame } from './TennisGame';
 
+const LOVE = 0;
+const FIFTEEN = 1;
+const THIRTY = 2;
+const FORTY = 3;
+const GAME = 4;
+
 const WINNING_SCORE = 4;
 const WIN_FOR_PLAYER1 = "Win for player1";
 const WIN_FOR_PLAYER2 = "Win for player2";
 const ADVANTAGE_PLAYER1 = "Advantage player1";
 const ADVANTAGE_PLAYER2 = "Advantage player2";
+
 const DEUCE = "Deuce";
 
+// Score names
+const LOVE_NAME = "Love";
+const FIFTEEN_NAME = "Fifteen";
+const THIRTY_NAME = "Thirty";
+const FORTY_NAME = "Forty";
 
 export class TennisGame5 implements TennisGame {
-  private readonly player2Name : string;
-  private readonly player1Name : string;
+  private readonly player2Name: string;
+  private readonly player1Name: string;
   private player1Score: number;
   private player2Score: number;
 
@@ -19,6 +31,21 @@ export class TennisGame5 implements TennisGame {
     this.player2Name = player2Name;
     this.player1Score = 0;
     this.player2Score = 0;
+  }
+
+  private buildScore(p1Score: number, p2Score: number): string {
+    const scoreNames: { [key: number]: string } = {
+      [LOVE]: LOVE_NAME,
+      [FIFTEEN]: FIFTEEN_NAME,
+      [THIRTY]: THIRTY_NAME,
+      [FORTY]: FORTY_NAME
+    };
+
+    // eslint-disable-next-line security/detect-object-injection
+    const p1ScoreName = scoreNames[p1Score];
+    // eslint-disable-next-line security/detect-object-injection
+    const p2ScoreName = scoreNames[p2Score];
+    return `${p1ScoreName}-${(p1ScoreName === p2ScoreName ) ? 'All' : p2ScoreName}`;
   }
 
   getScore(): string {
@@ -31,36 +58,46 @@ export class TennisGame5 implements TennisGame {
     }
 
     const lookup: { [key: number]: { [key: number]: string } } = {
-      0: {0: "Love-All",
-        1: "Love-Fifteen",
-        2: "Love-Thirty",
-        3: "Love-Forty",
-        4: WIN_FOR_PLAYER2},
+      [LOVE]: {
+        [LOVE]: this.buildScore(LOVE, LOVE),
+        [FIFTEEN]: this.buildScore(LOVE, FIFTEEN),
+        [THIRTY]: this.buildScore(LOVE, THIRTY),
+        [FORTY]: this.buildScore(LOVE, FORTY),
+        [GAME]: WIN_FOR_PLAYER2
+      },
 
-      1: {0: "Fifteen-Love",
-        1: "Fifteen-All",
-        2: "Fifteen-Thirty",
-        3: "Fifteen-Forty",
-        4: WIN_FOR_PLAYER2},
+      [FIFTEEN]: {
+        [LOVE]: this.buildScore(FIFTEEN, LOVE),
+        [FIFTEEN]: this.buildScore(FIFTEEN, FIFTEEN),
+        [THIRTY]: this.buildScore(FIFTEEN, THIRTY),
+        [FORTY]: this.buildScore(FIFTEEN, FORTY),
+        [GAME]: WIN_FOR_PLAYER2
+      },
 
-      2: {0: "Thirty-Love",
-        1: "Thirty-Fifteen",
-        2: "Thirty-All",
-        3: "Thirty-Forty",
-        4: WIN_FOR_PLAYER2},
+      [THIRTY]: {
+        [LOVE]: this.buildScore(THIRTY, LOVE),
+        [FIFTEEN]: this.buildScore(THIRTY, FIFTEEN),
+        [THIRTY]: this.buildScore(THIRTY, THIRTY),
+        [FORTY]: this.buildScore(THIRTY, FORTY),
+        [GAME]: WIN_FOR_PLAYER2
+      },
 
-      3: {0: "Forty-Love",
-        1: "Forty-Fifteen",
-        2: "Forty-Thirty",
-        3: DEUCE,
-        4: ADVANTAGE_PLAYER2},
+      [FORTY]: {
+        [LOVE]: this.buildScore(FORTY, LOVE),
+        [FIFTEEN]: this.buildScore(FORTY, FIFTEEN),
+        [THIRTY]: this.buildScore(FORTY, THIRTY),
+        [FORTY]: DEUCE,
+        [GAME]: ADVANTAGE_PLAYER2
+      },
 
-      4: {0: WIN_FOR_PLAYER1,
-        1: WIN_FOR_PLAYER1,
-        2: WIN_FOR_PLAYER1,
-        3: ADVANTAGE_PLAYER1,
-        4: DEUCE}
-    }
+      [GAME]: {
+        [LOVE]: WIN_FOR_PLAYER1,
+        [FIFTEEN]: WIN_FOR_PLAYER1,
+        [THIRTY]: WIN_FOR_PLAYER1,
+        [FORTY]: ADVANTAGE_PLAYER1,
+        [GAME]: DEUCE
+      }
+    };
 
     // eslint-disable-next-line security/detect-object-injection
     return lookup[p1][p2]
@@ -70,8 +107,8 @@ export class TennisGame5 implements TennisGame {
   wonPoint(playerName: string): void {
     if (playerName === this.player1Name) {
       this.player1Score += 1;
-    } 
-    if(playerName === this.player2Name) {
+    }
+    if (playerName === this.player2Name) {
       this.player2Score += 1;
     }
   }
